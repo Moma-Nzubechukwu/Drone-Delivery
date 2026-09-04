d3d87662eaa43d44f9b8b97bc2cb00cda51245dc
Moma-Nzubechukwu <momafransisco@gmail.com>
2026-09-04 04:40:51 +0100

    refactored my css code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,10 @@ const closeNavBtn = document.getElementById("close-nav");
 const nav = document.getElementById("navigation");
 const openNavBtn = document.getElementById('open-nav-btn');
 const innerNavSection = document.getElementById("inner-nav-section");
-closeNavBtn.addEventListener('click', ()=>{
-innerNavSection.classList.remove('nav-inner-active');
- nav.classList.remove("nav-active")
 
-
-});
 openNavBtn.addEventListener('click', ()=>{
-  innerNavSection.classList.add('nav-inner-active')
+  innerNavSection.classList.toggle('nav-inner-active')
   
-nav.classList.add('nav-active')
+nav.classList.toggle('nav-active')
 
 });
